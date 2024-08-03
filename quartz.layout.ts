a2ac09817ba34bf041ead66d54a71fcf1102fcdd
Diyaa Alkanakre <diyaa@diyaa.ca>
2024-08-03 13:13:58 -0600

    Quartz sync: Aug 3, 2024, 1:13 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "GitHub": "https://github.com/diyaa59",
+      "Linkedin": "https://www.linkedin.com/in/diyaa-alkanakre/",
     },
   }),
 }
