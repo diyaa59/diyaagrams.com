54b12a0b01031b7d6735cef975de6842f3faf611
Diyaa Alkanakre <diyaa@diyaa.ca>
2024-08-05 15:16:15 -0600

    Quartz sync: Aug 5, 2024, 3:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -54,7 +54,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
+        priority: ["frontmatter", "git", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
