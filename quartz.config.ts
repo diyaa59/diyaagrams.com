8678c0c210e64f623865b05530ba9a2435996250
Diyaa Alkanakre <diyaa@diyaa.ca>
2024-08-05 22:18:59 -0600

    Quartz sync: Aug 5, 2024, 10:18 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -97,6 +97,7 @@ const config: QuartzConfig = {
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: true,
+        rssLimit: 20,
       }),
       Plugin.Assets(),
       Plugin.Static(),
