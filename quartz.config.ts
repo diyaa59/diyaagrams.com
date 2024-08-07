f44acd504854b81a06c2b7c1ab806be92f5895d7
Diyaa Alkanakre <diyaa@diyaa.ca>
2024-08-07 09:18:57 -0600

    Quartz sync: Aug 7, 2024, 9:18 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "diyaagrams.com",
     ignorePatterns: ["private"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
